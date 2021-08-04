9f9733fe5dbc0740bf0491b038649ca37257f54c
Chung Tran <bigearsenal@gmail.com>
2021-08-04 14:20:50 +0700

    Add some models

---
+++ RenSwift/Classes/Models/BN.swift
@@ -0,0 +1,13 @@
+//
+//  BN.swift
+//  RenSwift
+//
+//  Created by Chung Tran on 04/08/2021.
+//
+
+import Foundation
+
+protocol BN {
+    func toString() -> String
+    func isBN(b: Any) -> Bool
+}
