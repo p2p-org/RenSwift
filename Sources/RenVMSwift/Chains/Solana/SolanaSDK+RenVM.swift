4ee6c4340603e021eac6a733cb23424f01e40530
Chung Tran <bigearsenal@gmail.com>
2022-02-14 10:57:27 +0700

    feat: add source files

---
+++ Sources/RenVMSwift/Chains/Solana/SolanaSDK+RenVM.swift
@@ -0,0 +1,11 @@
+//
+//  SolanaSDK+RenVM.swift
+//  SolanaSwift
+//
+//  Created by Chung Tran on 15/09/2021.
+//
+
+import Foundation
+import SolanaSwift
+
+extension SolanaSDK: RenVMSolanaAPIClientType {}
--- Sources/RenVMSwift/RenVMSwift.swift
+++
@@ -1,6 +0,0 @@
-public struct RenVMSwift {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
