0cfcaafdfeec34869983fb2a650d9e235d71dc9c
Chung Tran <bigearsenal@gmail.com>
2022-05-19 18:20:55 +0700

    fix: add integration test

--- Tests/IntegrationTests/Actions/BurnAndReleaseTests.swift
+++ Tests/IntegrationTests/Actions/BurnAndReleaseTests.swift
@@ -1,5 +1,7 @@
 import Foundation
 import XCTest
+import RenVMSwift
+import SolanaSwift
 
 class BurnAndReleaseTests: XCTestCase {
     //    override var endpoint: APIEndPoint {
@@ -13,11 +15,21 @@ class BurnAndReleaseTests: XCTestCase {
     
     func testBurnAndRelease() async throws {
         let rpcClient = RpcClient(network: .testnet)
+        let solanaAPIClient = JSONRPCAPIClient(
+            endpoint: .init(
+                address: "https://api.mainnet-beta.solana.com",
+                network: .mainnetBeta
+            )
+        )
 
-        let solanaChain = try await SolanaChain.load(client: rpcClient, solanaClient: solanaSDK)
+        let solanaChain = try await SolanaChain.load(
+            client: rpcClient,
+            apiClient: solanaAPIClient,
+            blockchainClient: BlockchainClient(apiClient: solanaAPIClient)
+        )
 
+        let account = try await Account(phrase: "matter outer client aspect pear cigar caution robust easily merge dwarf wide short sail unusual indicate roast giraffe clay meat crowd exile curious vibrant".components(separatedBy: " "), network: .testnet)
         let recipient = "tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt"
-
         let amount = "1000"
 
         let burnAndRelease = BurnAndRelease(
@@ -28,23 +40,19 @@ class BurnAndReleaseTests: XCTestCase {
             burnTo: "Bitcoin"
         )
 
-//        let detail = try burnAndRelease.submitBurnTransaction(
-//            account: account.publicKey.data,
-//            amount: amount,
-//            recipient: recipient,
-//            signer: account.secretKey
-//        ).toBlocking().first()!
-
-        let detail: BurnAndRelease.BurnDetails = .init(
-            confirmedSignature: "5Dmpba9yiJSyGUejRveSz1aS463Qj1s3oeV1JT4VKmrPgQsKFyikArLuFSihBGsG9yYybEKkawFFAnx7pajLtE1K",
-            nonce: 56,
+        let detail = try await burnAndRelease.submitBurnTransaction(
+            account: account.publicKey.data,
+            amount: amount,
             recipient: recipient,
-            amount: amount
+            signer: account.secretKey
         )
-//
+        
+        try await solanaAPIClient.waitForConfirmation(signature: detail.confirmedSignature, ignoreStatus: true)
+        
         let burnState = try burnAndRelease.getBurnState(burnDetails: detail)
 
-        let tx = try burnAndRelease.release(state: burnState, details: detail).toBlocking().first()
-
+        let tx = try await burnAndRelease.release(state: burnState, details: detail)
+        
+        print(tx)
     }
 }
